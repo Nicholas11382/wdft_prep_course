565bc3edff1d3ec783b5d893f852b9d6e4ad9525
Nicholas Losco <30914365+Nicholas11382@users.noreply.github.com>
2017-09-20 15:49:49 -0400

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,26 @@
+/*
+Exercise #1
+Create a for loop that prints out the numbers 1 to 100 in the console.
+*/
+for (var i = 0;i < 101; - i++){
+    console.log(i);
+}
+console.log('loop is done');
+var hashStr = '#';
+var outputStr = '';
+for(var i = 0; i < 7; i++){
+  outputStr = outputStr + hashStr;
+  console.log(outputStr);
+}
+/*
+Exercise #2
+Write a loop that makes seven calls to console.log to output the following triangle.
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
